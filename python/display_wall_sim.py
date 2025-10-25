c9e2bd3b53cfe7c54e42bd2676884ef1a6ddb6c3
Aaron Yoon <awesomeyooner@gmail.com>
2025-10-25 01:54:16 -0700

    updated the main exec to remove constants and use the Wall class

--- python/display_wall_sim.py
+++ python/display_wall_sim.py
@@ -1,24 +1,13 @@
 import pygame
 from pymathlib import util
+from wall_sim.wall import Wall
+from wall_sim.constants import *
+import colors
 
-VIEW_WIDTH = 800 # pixels
-VIEW_HEIGHT = 600 # pixesl
-
-util.width = VIEW_WIDTH
-util.height = VIEW_HEIGHT
-
-CANVAS_WIDTH = 3 # meters
-CANVAS_HEIGHT = 3 # meters
-
-PAINTBALL_RADIUS = 0.017272 # meters
-PAINTBALL_SPLATTER_RADIUS = 0.02 # meters
-
-METERS_PER_PIXEL = VIEW_WIDTH / CANVAS_WIDTH
-
-BACKGROUND_COLOR = (255, 255, 255)
-
-screen = pygame.display.set_mode((VIEW_WIDTH, VIEW_HEIGHT))
+screen = pygame.display.set_mode((CANVAS_WIDTH, CANVAS_HEIGHT))
 pygame.display.set_caption("Wall Simulation")
+
+wall = Wall((WALL_WIDTH, WALL_HEIGHT), (CANVAS_WIDTH, CANVAS_HEIGHT), WALL_DISTANCE)
 
 keep_alive = True
 clock = pygame.time.Clock()
@@ -30,7 +19,8 @@ while keep_alive:
             keep_alive = False
 
     screen.fill(BACKGROUND_COLOR)
-    # pygame.draw.rect(screen, (0, 0, 200, 200))
+    # pygame.draw.rect(screen, WALL_COLOR, (0, 0, 200, 200))
+    wall.draw_wall(screen)
     center = util.cartesian_to_native(0, 0)
     pygame.draw.circle(screen, (255, 0, 0), center, 40)
 
